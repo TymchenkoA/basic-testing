89277a767d30b8ef2c818396968140108db72043
Anastasiia Tymchenko <tymchenko.anastasiia@gmail.com>
2024-10-19 08:26:00 +0100

    feat: implement mocking lib api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,58 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi  } from './index'; 
+
+jest.mock('axios');
 
 describe('throttledGetDataFromApi', () => {
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.useFakeTimers(); 
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const mockGet = jest.fn().mockResolvedValue({ data: {} });
+    const mockCreate = axios.create as jest.Mock;
+    mockCreate.mockReturnValue({ get: mockGet });
+
+    await throttledGetDataFromApi('/guide');
+
+    expect(mockCreate).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    // const mockGet = jest.fn().mockResolvedValue({ data: {} });
+    // (axios.create as jest.Mock).mockReturnValue({ get: mockGet });
+
+    // const relativePath = '/guide';
+ 
+    // throttledGetDataFromApi(relativePath);
+    // jest.advanceTimersByTime(5000);
+    // await Promise.resolve();
+    
+    // expect(mockGet).toHaveBeenCalledWith(relativePath);
+
   });
 
   test('should return response data', async () => {
-    // Write your test here
+  //   const mockData = { name: 'Bob' };
+  //   const mockGet = jest.fn().mockResolvedValue({ data: mockData });
+  //   (axios.create as jest.Mock).mockReturnValue({ get: mockGet });
+
+  //   const relativePath = '/guide';
+
+  //   const result = await throttledGetDataFromApi(relativePath);
+
+  //   jest.advanceTimersByTime(5000);
+  //   await Promise.resolve();
+
+  //   expect(result).toEqual(mockData); 
   });
 });
